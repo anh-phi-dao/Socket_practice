8f53ff8b7795d84b1336ce43c5334c3507ed3c95
DaoAnhPhi-Gigabyte-PC <freeakaphi2k3@gmail.com>
2025-10-26 12:33:55 +0700

    Add comment

--- include/client_udp.h
+++ include/client_udp.h
@@ -14,8 +14,21 @@ extern int client_udp_fd;
 extern struct sockaddr_in server;
 
 extern socklen_t len;
-
+/**
+ * @brief Create UDP client
+ * @param client_fd: a pointer which point to a file descriptor coresponding to client UDP socket.
+ * User can use predefined int client_udp_fd
+ * @return ERROR = -1, SUCCESS = 0
+ */
 int create_UDP_IPv4_client(int *client_fd);
+/**
+ * @brief Create server address for server UDP connection
+ * @param client_addr: pointer to server address following IPv4. The port and IPv4 information will be stored in this strruct
+ * in network format.
+ * @param IPv4: a pointer pointing to a char array which contains IPv4 address in human-reable string
+ * @param port: port number, this is a parameter belongs to transport layer of OSI model. User can use predefined PORT =8000
+ * @return SUCCESS=0, ERROR=-1
+ */
 int create_UDP_IPv4_server_address(struct sockaddr_in *server_addr, char *IPv4, uint16_t port);
 
 enum
